04e1506b135cf6a967d802d9a0d7af1f77af8456
Matthew Krichmar <matthewkrichmar@matthews-air.myfiosgateway.com>
2024-02-28 17:09:59 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Nav from './components/Nav'
+import Section from './components/Section'
+
+function App() {
+
+  return (
+    <div id="container">
+      <Nav />
+      <Section />
+    </div>
+  )
+}
+
+export default App
